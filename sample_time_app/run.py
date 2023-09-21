f6d78500b09344317809ddd434f85b8146b4d8d7
Iris <i5052637@gmail.com>
2023-09-21 15:22:10 -0400

    add return current time feature

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
+
+
+@app.route('/time')
+def current_time():
+    date = datetime.now()
+    return str(date)
 
 
 @app.route('/')
